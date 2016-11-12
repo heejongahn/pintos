4a65f28bff89b9dd1b7272ba8f6d572876edd54c
Heejong Ahn <heejongahn@gmail.com>
2016-11-12 17:33:06 +0300

    Add s_page_inser_file function

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,3 +1,4 @@
+#include "threads/malloc.h"
 #include "vm/page.h"
 
 static unsigned
@@ -16,4 +17,27 @@ less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux) {
 void
 s_page_init () {
   hash_init (&s_page_table, &hash_func, &less_func, NULL);
+}
+
+bool
+s_page_insert_file (uint8_t *uaddr, struct file *file, off_t ofs,
+    uint32_t read_bytes, uint32_t zero_bytes, bool writable) {
+  struct s_page *s_page = malloc (sizeof (struct s_page));
+
+  s_page->uaddr = uaddr;
+  s_page->location = DISK;
+
+  s_page->file_info.file = file;
+  s_page->file_info.ofs = ofs;
+  s_page->file_info.read_bytes = read_bytes;
+  s_page->file_info.zero_bytes = zero_bytes;
+  s_page->file_info.writable = writable;
+
+  struct hash_elem *success = hash_insert (&s_page_table, &s_page->h_elem);
+
+  if (!success) {
+    return true;
+  }
+
+  return false;
 }
--- src/vm/page.h
+++ src/vm/page.h
@@ -1,7 +1,7 @@
-#include "hash.h"
+#include <hash.h>
+#include "filesys/file.h"
 
 enum page_location {
-  FRAME,
   DISK,
   SWAP,
   ZERO
@@ -13,7 +13,19 @@ struct s_page {
   uint8_t *uaddr;
   enum page_location location;
 
+  struct {
+    /* Info for file page */
+    struct file *file;
+    off_t ofs;
+    uint32_t read_bytes;
+    uint32_t zero_bytes;
+    bool writable;
+  } file_info;
+
+  /* Info for swap page */
+
   struct hash_elem h_elem;
 };
 
 void s_page_init (void);
+bool s_page_insert_file (uint8_t *, struct file *, off_t, uint32_t, uint32_t, bool);
