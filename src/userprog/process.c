0dab98100198f4ad98635c8369dac7e20ff7ed83
Heejong Ahn <heejongahn@gmail.com>
2016-11-14 14:58:40 +0300

    Debug broken ofs handling in load_segment
    With lazy loading, we need to manually advance ofs by PGSIZE.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -451,8 +451,8 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
   ASSERT (ofs % PGSIZE == 0);
 
   bool success = true;
-
   file_seek (file, ofs);
+
   while (read_bytes > 0 || zero_bytes > 0)
     {
       /* Do calculate how to fill this page.
@@ -461,18 +461,39 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
-      /* Just add to the supplemental page table and that's it*/
+      /*
+      uint8_t *kpage = palloc_get_page (PAL_USER);
+      if (kpage == NULL)
+        return false;
+
+      printf("%p, %p, %d, %d, %d\n", file, upage,
+          page_read_bytes, page_zero_bytes, writable);
+
+      if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
+        {
+          palloc_free_page (kpage);
+          return false;
+        }
+      memset (kpage + page_read_bytes, 0, page_zero_bytes);
+
+      if (!install_page (upage, kpage, writable))
+        {
+          palloc_free_page (kpage);
+          return false;
+        }
+      */
+
       if (page_zero_bytes == PGSIZE) {
         success = s_page_insert_zero (upage);
       } else {
         success = s_page_insert_file (upage, file, ofs, page_read_bytes, page_zero_bytes, writable);
+        ofs = ofs += PGSIZE;
       }
 
       if (!success) {
         return false;
       }
 
-      /* Advance. */
       read_bytes -= page_read_bytes;
       zero_bytes -= page_zero_bytes;
       upage += PGSIZE;
