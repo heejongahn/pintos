fdc6de4e5707b9093a6ddb35b5d9fc5a692b17b9
Heejong Ahn <heejongahn@gmail.com>
2016-11-15 10:10:45 +0300

    Implement prototype swap_in

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -2,9 +2,35 @@
 #include "threads/vaddr.h"
 #include "vm/swap.h"
 
+#define SECTOR_PER_PAGE (PGSIZE / DISK_SECTOR_SIZE)
+
 void
 swap_init () {
-  int swap_pages = disk_size (disk_get (1,1)) / (PGSIZE / DISK_SECTOR_SIZE);
+  swap_disk = disk_get (1,1);
+  swap_table = bitmap_create (disk_size (swap_disk) / SECTOR_PER_PAGE);
+}
 
-  swap_table = bitmap_create (swap_pages);
+bool
+swap_in (struct s_page *page) {
+  int i;
+  void *buffer;
+
+  // Find free swap slot
+  size_t swap_idx = bitmap_scan_and_flip (swap_table, 0, 1, 0);
+  if (swap_idx == BITMAP_ERROR) {
+    return false;
+  }
+
+  // Actually writing into the disk
+  disk_sector_t start_sector = swap_idx * SECTOR_PER_PAGE;
+
+  for (i=0; i<SECTOR_PER_PAGE; i++) {
+    buffer = page->uaddr + (DISK_SECTOR_SIZE / sizeof (uint8_t *));
+    disk_write (swap_disk, start_sector + i, buffer);
+  }
+
+  // Add to s_page_table
+  s_page_insert_swap (page->uaddr, page->writable, swap_idx);
+
+  return true;
 }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -2,12 +2,14 @@
 #include "vm/page.h"
 #include "devices/disk.h"
 
+static struct disk *swap_disk;
 static struct bitmap *swap_table;
 
 void swap_init (void);
 
+bool swap_in (struct s_page *);
+
 /*
-bool swap_in (uint8_t *);
 bool swap_out (uint8_t *);
 bool swap_find (
 */
